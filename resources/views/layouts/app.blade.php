a502dc2ddb808770300e35d27228ae1bec497e23
Jeffrey Koper <jkoper257@student.novacollege.nl>
2023-06-18 00:16:44 +0200

    finishing touches

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,12 +34,12 @@
                             </ul>
                         </li>
                         
+                        @auth
                         <li class="nav-item">
                             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                                 {{ __('Dashboard') }}
                             </x-responsive-nav-link>
                         </li>
-                        @auth
                         <li class="nav-item">
                             <x-responsive-nav-link :href="route('profile.edit')">
                                 {{ __('Profile') }}
